29e4aba941c95bf003123db0459a9ef6356afae1
Shiera <shiera@example.com>
2015-04-01 16:03:31 +0300

    generate class and reference interface

--- src/main/java/miniprojekti/reference/Reference.java
+++ src/main/java/miniprojekti/reference/Reference.java
@@ -1,7 +1,22 @@
 package miniprojekti.reference;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by cec on 1.4.2015.
  */
 public interface Reference {
+    /*
+      2 Lists of enums what data this kind of reference must an can contain
+     */
+    List<Enum> getOptionalReferenceParts();
+    List<Enum> getMandatoryReferenceParts();
+
+    /*
+    map where the inputted data is saved
+     */
+    Map<Enum, String> getReferenceParts();
+
+
 }
--- src/main/java/miniprojekti/reference/generate/BibTexGenerator.java
+++ src/main/java/miniprojekti/reference/generate/BibTexGenerator.java
@@ -1,7 +1,32 @@
 package miniprojekti.reference.generate;
 
+import miniprojekti.reference.Reference;
+
 /**
  * Created by cec on 1.4.2015.
  */
 public class BibTexGenerator {
+    private Reference reference;
+
+    //Todo should it be able to take a list of references and generate one whole document
+    public BibTexGenerator(Reference reference){
+        this.reference = reference;
+    }
+
+    public void generate(){
+        String bibTex = "";
+        for (Enum part : reference.getReferenceParts().keySet()) {
+            generateLine(part.toString(), reference.getReferenceParts().get(part));
+        }
+
+    }
+
+
+
+    private String generateLine(String whatToGenerate, String dataField){
+        return "  " + whatToGenerate + " = {" + dataField + "},\n";
+    }
+
+
+
 }
