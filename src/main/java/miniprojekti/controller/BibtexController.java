27119267c9ff7d0941f5e2c1d7e882bd64b59fff
Ville-Veikko Saari <ville-veikko.saari@cs.helsinki.fi>
2015-04-09 16:06:11 +0300

    BibtexController

--- src/main/java/miniprojekti/controller/BibtexController.java
+++ src/main/java/miniprojekti/controller/BibtexController.java
@@ -5,44 +5,75 @@
  */
 package miniprojekti.controller;
 
+import miniprojekti.reference.*;
 import miniprojekti.repository.ReferenceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+
+import static miniprojekti.reference.EntryType.*;
+import static miniprojekti.reference.BibTexType.*;
+
 /**
- *
  * @author vrsaari
  */
 @Controller
 public class BibtexController {
 
+    @Autowired
     private ReferenceRepository referenceRepository;
 
     @RequestMapping("/bibtex")
     public String list(Model model) {
-        model.addAttribute("lista", referenceRepository.findAll());
+        List<AbstractReference> references = new ArrayList<AbstractReference>();
+        EnumMap<EntryType, String> sisalto = new EnumMap<EntryType, String>(EntryType.class);
+
+        references.add(new ArticleReference(
+                        "nimi",
+                        sisalto
+                )
+        );
+        references.add(new ArticleReference(
+                        "toinen nimi",
+                        sisalto
+                )
+        );
+        List<AbstractReference> reposta = referenceRepository.findAll();
+        if (reposta != null || reposta.size() < 1) {
+            for (AbstractReference ref : reposta) {
+                if (ref != null) {
+                    references.add(ref);
+                }
+            }
+        }
+        model.addAttribute("lista", references);
 
         return "list";
     }
-    
+
     @RequestMapping("/bibtex/{referenceId}")
     public String view(Model model) {
-        
+
         return "view";
     }
-    
+
     @RequestMapping(value = "/bibtex", method = RequestMethod.POST)
     public String add() {
-        
+
         return "redirect:/bibtex/";
     }
-    
+
     @RequestMapping(value = "/bibtex/{referenceId}", method = RequestMethod.POST)
     public String edit() {
-        
+
         return "redirect:/bibtex/";
     }
 }
--- src/main/java/miniprojekti/repository/ReferenceRepository.java
+++ src/main/java/miniprojekti/repository/ReferenceRepository.java
@@ -4,10 +4,12 @@ package miniprojekti.repository;
 import miniprojekti.reference.AbstractReference;
 import miniprojekti.reference.Reference;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by jani on 9.4.2015.
  */
+@Repository
 public interface ReferenceRepository extends JpaRepository<AbstractReference, Long> {
 
 
