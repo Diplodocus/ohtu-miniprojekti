782e136647712440f3f194339e4594ba834d5901
Jani Luukko <jj.luukko@gmail.com>
2015-04-09 14:49:15 +0300

    db muutos

--- src/main/java/miniprojekti/repository/ReferenceRepository.java
+++ src/main/java/miniprojekti/repository/ReferenceRepository.java
@@ -1,12 +1,13 @@
 package miniprojekti.repository;
 
-import miniprojekti.reference.AbstractReference;
+// import miniprojekti.reference.AbstractReference;
+import miniprojekti.reference.Reference;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by jani on 9.4.2015.
  */
-public interface ReferenceRepository extends JpaRepository<AbstractReference, Long> {
+public interface ReferenceRepository extends JpaRepository<Reference, Long> {
 
 
 }
