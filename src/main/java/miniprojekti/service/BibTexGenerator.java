b6aa20e1fcc694a2f37a4b9b00fe93c28ca60b46
Ville-Veikko Saari <ville-veikko.saari@cs.helsinki.fi>
2015-04-27 15:03:11 +0300

    encoding fix ja tyhjiä kenttiä ei generoida bibtexiin

--- src/main/java/miniprojekti/service/BibTexGenerator.java
+++ src/main/java/miniprojekti/service/BibTexGenerator.java
@@ -7,18 +7,18 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 
 /**
-
+ *
  */
 @Service
 public class BibTexGenerator {
 
-
     /**
      * Generates the bibTex as a String from a reference
+     *
      * @param referenceInterface reference to be generated
      * @return reference as bibTex string
      */
-    public String generate(ReferenceInterface referenceInterface){
+    public String generate(ReferenceInterface referenceInterface) {
         StringBuilder bibTex = new StringBuilder();
         bibTex.append("@")
                 .append(referenceInterface.getType().getName())
@@ -26,8 +26,10 @@ public class BibTexGenerator {
                 .append(referenceInterface.getName());
 
         for (EntryType part : referenceInterface.getEntries().keySet()) {
-            bibTex.append(",\n");
-            generateLine(part.getName(), referenceInterface.getEntries().get(part), bibTex);
+            if (!referenceInterface.getEntries().get(part).isEmpty()) {
+                bibTex.append(",\n");
+                generateLine(part.getName(), referenceInterface.getEntries().get(part), bibTex);
+            }
         }
 
         bibTex.append("\n}\n");
@@ -35,7 +37,7 @@ public class BibTexGenerator {
 
     }
 
-    public String generateAll(ArrayList<ReferenceInterface> referenceInterfaces){
+    public String generateAll(ArrayList<ReferenceInterface> referenceInterfaces) {
 
         StringBuilder wholeBibTex = new StringBuilder();
         BibTexGenerator generator = new BibTexGenerator();
@@ -48,17 +50,14 @@ public class BibTexGenerator {
         return wholeBibTex.toString();
     }
 
-
     /**
      *
      * @param whatToGenerate type of the entry as a string
      * @param dataField data for that entry
      * @param bibTex the StringBuilder where it's added
      */
-    private void generateLine(String whatToGenerate, String dataField, StringBuilder bibTex){
+    private void generateLine(String whatToGenerate, String dataField, StringBuilder bibTex) {
         bibTex.append("  ").append(whatToGenerate).append(" = {").append(dataField).append("}");
     }
-
-
 
 }
