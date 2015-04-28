c9f2800a94a9896af0454f15ebedb187a1d80b61
Zonsu <johanna.paavola@helsinki.fi>
2015-04-29 01:16:05 +0300

    book reference junit file moved to domain package

--- src/test/java/miniprojekti/domain/BookReferenceInterfaceTest.java
+++ src/test/java/miniprojekti/domain/BookReferenceInterfaceTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package miniprojekti.reference;
+package miniprojekti.domain;
 
 import org.junit.After;
 import org.junit.AfterClass;
